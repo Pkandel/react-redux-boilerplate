36cfcde3d23945d002d619daac2ca1272b4accc7
prakash <unique_prakash2002@yahoo.com>
2017-07-16 14:29:24 +1000

    deleted unnecessary code from nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -47,37 +47,11 @@ profile = () => {
                 {title}
             </button>
         );
-        const Account = ({ title, history }) => (
-            <button
-                type="button"
-                onClick={() => history.push('/account')}
-            >
-                {title}
-            </button>
-        );
-        const DropdownComponent = ({ history }) => (
-            <Dropdown
-                type="button"
-                user={user}
-                dropdownListItem={[{
-                    name: "account",
-                    onClick: () => history.push('/account'),
-                    icon: "settings"
-                },
-                {
-                    name: "profile",
-                    onClick: () => history.push('/profile'),
-                    icon: "exit_to_app"
-                }]}
-            />
-
-        );
         return (
             <div className="navbar">
                 <li><NavLink exact to="/" activeClassName="active">Home </NavLink></li>
                 <li><NavLink exact to="/dashboard" activeClassName="active">Dashboard </NavLink></li>
                 <li><Route path="/" render={(props) => <ButtonToNavigate {...props} title="Button as NavLivk" />} /></li>
-                {/* <li><Route path="/" render={(props) => <DropdownComponent {...props} />} /></li> */}
                 <li><Dropdown user={user} dropdownListItem={dropdownListItem} className={className} /></li>
             </div>
                 );
