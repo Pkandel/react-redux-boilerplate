3237bd3b0901d55b896082acea36b442a94126f3
Prakash Kandel <unique_prakash2002@yahoo.com>
2017-07-18 15:55:36 +1000

    added sample sidebar exapple

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,7 +6,7 @@ class Home extends Component {
     render() {
         return(
             <div className="navbar">
-                <h1> this is Home!!!</h1>
+                <h1> this is Home</h1>
             </div>
         );
     }
