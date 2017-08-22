ce7b5c5c85ace637ff7263a833ef50befcb8738e
Prakash Kandel <unique_prakash2002@yahoo.com>
2017-08-22 23:30:00 +1000

    modified some setting

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,17 +1,16 @@
-"use strict";
 import React, { Component } from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 class Root extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <h1> React Redux Boilerplate
-                {JSON.stringify(process.env)}
-                </h1>
-            </BrowserRouter>
-        );
-    }
+  render () {
+    return (
+      <BrowserRouter>
+        <h1> React Redux Boilerplate
+          {JSON.stringify(process.env)}
+        </h1>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default Root;
--- webpack.config.js
+++ webpack.config.js
@@ -6,113 +6,112 @@ const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const env = process.env.NODE_ENV || "production";
+const env = process.env.NODE_ENV || 'production';
 console.log(`Application running in ${env} mode`);
 /*
     to send a production variable, we have to generate production before
     application statrs like NODE_ENV=production npm start
     other env variable we can use from .env file
 */
-const dev = env === "development";
+const dev = env === 'development';
 
 module.exports = {
-    entry: dev
-        ? [
-            'webpack-hot-middleware/client?reload=true',
-            path.resolve(__dirname, "src")
-        ] : path.resolve(__dirname, "src"),
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-        publicPath: '/'
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, dev ? 'src' : 'dist')
+  entry: dev
+    ? [
+      'webpack-hot-middleware/client?reload=true',
+      path.resolve(__dirname, 'src')
+    ] : path.resolve(__dirname, 'src'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, dev ? 'src' : 'dist')
 
-    },
-    target: 'web',
-    devtool: dev ? 'cheap-module-inline-source-map' : 'source-map',
-    plugins: dev ? [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new LodashModuleReplacementPlugin,
-        new Dotenv()
+  },
+  target: 'web',
+  devtool: dev ? 'cheap-module-inline-source-map' : 'source-map',
+  plugins: dev ? [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new LodashModuleReplacementPlugin(),
+    new Dotenv()
 
-    ] : [
-            new webpack.optimize.OccurrenceOrderPlugin(),
-            new ExtractTextPlugin("styles.css"),
-            new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-            new CopyWebpackPlugin([
-                { from: 'public/', to: './' }
-            ]),
-            new Dotenv()
+  ] : [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+    new CopyWebpackPlugin([
+      { from: 'public/', to: './' }
+    ]),
+    new Dotenv()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'src')
         ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                include: [
-                    path.resolve(__dirname, 'src')
-                ],
-                exclude: [
-                    path.resolve(__dirname, 'node_modules/'),
-                    path.resolve(__dirname, 'dist/')
-                ],
-                use: [
-                    'react-hot-loader',
-                    {
-                        loader: 'babel-loader',
-                    }
-                ]
-            },
-            {
-                test: /\.scss?$/,
-                include: [
-                    path.resolve(__dirname, 'src')
-                ],
-                exclude: [
-                    path.resolve(__dirname, 'node_modules'),
-                    path.resolve(__dirname, 'build')
-                ],
-                use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {
-                    loader: "sass-loader" // compiles Sass to CSS
-                }
-                ]
-            },
-            {
-                test: /\.css?$/,
-                include: [
-                    path.resolve(__dirname, 'src')
-                ],
-                exclude: [
-                    path.resolve(__dirname, 'node_modules'),
-                    path.resolve(__dirname, 'build')
-                ],
-                use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }
-                ]
-            }
+        exclude: [
+          path.resolve(__dirname, 'node_modules/'),
+          path.resolve(__dirname, 'dist/')
+        ],
+        use: [
+          'react-hot-loader',
+          {
+            loader: 'babel-loader'
+          }
         ]
-    },
-    resolve: {
-        modules: [
-            'node_modules',
-            path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.scss?$/,
+        include: [
+          path.resolve(__dirname, 'src')
         ],
-        extensions: ['.js', '.jsx', '.json', '.css', '.sass'],
-        root: path.resolve('./src'),
-        alias: {
-            //to use alias support in Webstorm we have to mark src as mark directory as resource root
-            mock_api: path.resolve(__dirname, 'src/api/mock_api'),
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'build')
+        ],
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
         }
-
+        ]
+      },
+      {
+        test: /\.css?$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'build')
+        ],
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, 'src')
+    ],
+    extensions: ['.js', '.jsx', '.json', '.css', '.sass'],
+    alias: {
+      // to use alias support in Webstorm we have to mark src as mark directory as resource root
+      mock_api: path.resolve(__dirname, 'src/api/mock_api')
     }
+
+  }
 
 };
