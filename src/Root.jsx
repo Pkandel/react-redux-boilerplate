f7b6e79415dee7180cb241547682bcd44e11c204
Prakash Kandel <prakash.kandel@strategicdata.com.au>
2019-05-07 15:46:13 +1000

    Update packages to latest and add webpack-dev-server and remove express

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 class Root extends Component {
-  render () {
-    return (
-      <BrowserRouter>
-        <h1> React Redux Boilerplate
-          {JSON.stringify(process.env)}
-        </h1>
-      </BrowserRouter>
-    );
-  }
+	render () {
+		return (
+			<BrowserRouter>
+				<h1> React Redux boilerplate Superb!!! </h1>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default Root;
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,53 +1,16 @@
 /* eslint-disable no-console */
-import path from  'path';
-import webpack from  'webpack';
-import Dotenv from  'dotenv-webpack';
-import LodashModuleReplacementPlugin from  'lodash-webpack-plugin';
-import ExtractTextPlugin from  'extract-text-webpack-plugin';
-import CopyWebpackPlugin from  'copy-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import merge from 'webpack-merge';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const env = process.env.NODE_ENV || 'production';
-console.log(`Application running in ${env} mode`);
-const dev = env === 'development';
-module.exports = {
-	entry: dev
-		? [
-			'webpack-hot-middleware/client?reload=true',
-			'react-hot-loader/patch',
-			path.resolve(__dirname, 'src')
-		] : path.resolve(__dirname, 'src'),
+const commonConfig = {
+	entry: path.resolve(__dirname, 'src'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/'
+		filename: 'js/[name].js'
 	},
-	devServer: {
-		contentBase: path.resolve(__dirname, dev ? 'src' : 'dist')
-
-	},
-	target: 'web',
-	devtool: dev ? 'cheap-module-inline-source-map' : 'source-map',
-	plugins: dev ? [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoEmitOnErrorsPlugin(),
-		new LodashModuleReplacementPlugin(),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('development')
-		}),
-		new Dotenv()
-
-	] : [
-		new webpack.optimize.OccurrenceOrderPlugin(),
-		new ExtractTextPlugin('styles.css'),
-		new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('production')
-		}),
-		new CopyWebpackPlugin([
-			{ from: 'public/', to: './' }
-		]),
-		new Dotenv()
-	],
 	module: {
 		rules: [
 			{
@@ -56,10 +19,11 @@ module.exports = {
 					path.resolve(__dirname, 'src')
 				],
 				exclude: [
-					path.resolve(__dirname, 'node_modules/'),
-					path.resolve(__dirname, 'dist/')
+					path.resolve(__dirname, 'node_modules/')
 				],
-				use: dev ? ['react-hot-loader/webpack', 'babel-loader'] : 'babel-loader'
+				use: {
+					loader: 'babel-loader'
+				}
 			},
 			{
 				test: /\.scss?$/,
@@ -67,8 +31,7 @@ module.exports = {
 					path.resolve(__dirname, 'src')
 				],
 				exclude: [
-					path.resolve(__dirname, 'node_modules'),
-					path.resolve(__dirname, 'build')
+					path.resolve(__dirname, 'node_modules')
 				],
 				use: [{
 					loader: 'style-loader' // creates style nodes from JS strings
@@ -76,8 +39,7 @@ module.exports = {
 					loader: 'css-loader' // translates CSS into CommonJS
 				}, {
 					loader: 'sass-loader' // compiles Sass to CSS
-				}
-				]
+				}]
 			},
 			{
 				test: /\.css?$/,
@@ -85,18 +47,30 @@ module.exports = {
 					path.resolve(__dirname, 'src')
 				],
 				exclude: [
-					path.resolve(__dirname, 'node_modules'),
-					path.resolve(__dirname, 'build')
+					path.resolve(__dirname, 'node_modules')
 				],
 				use: [{
 					loader: 'style-loader' // creates style nodes from JS strings
 				}, {
 					loader: 'css-loader' // translates CSS into CommonJS
-				}
-				]
+				}]
 			}
 		]
 	},
+	plugins: [
+		// this will see the output path of webpack and copy index.html to that place
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public/index.html'),
+			inject: 'body'
+		}),
+		new CopyWebpackPlugin([
+			{
+				from: path.resolve(__dirname, 'public/'),
+				to: path.resolve(__dirname, 'dist/')
+			}
+		])
+	],
+
 	resolve: {
 		modules: [
 			path.resolve(__dirname, 'node_modules'),
@@ -105,9 +79,34 @@ module.exports = {
 		extensions: ['.js', '.jsx', '.json', '.css', '.scss'],
 		alias: {
 			// to use alias support in Webstorm we have to mark src as mark directory as resource root
-			mock_api: path.resolve(__dirname, 'src/api/mock_api')
+			src: path.resolve(__dirname, 'src')
 		}
 
 	}
+};
 
+const devConfig = merge(commonConfig, {
+	devtool: 'cheap-module-inline-source-map',
+	mode: 'development',
+	devServer: {
+		contentBase: path.resolve(__dirname, 'public/'),
+		stats: 'minimal',
+		port: 9000,
+		open: true,
+		historyApiFallback: true,
+		disableHostCheck: true
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
+});
+const prodConfig = merge(commonConfig, {
+	devtool: 'source-map',
+	mode: 'production'
+});
+
+
+module.exports = (env) => {
+	console.log('Application is running in ' + env.NODE_ENV);
+	return env.NODE_ENV === 'development' ? devConfig : prodConfig;
 };
