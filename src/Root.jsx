aa49939a754ea155205782cf3e7c401308f7d714
Prakash Kandel <unique_prakash2002@yahoo.com>
2019-05-13 22:08:53 +1000

    Initial commit

--- src/Root.jsx
+++ src/Root.jsx
@@ -7,10 +7,10 @@ class Root extends Component {
 	render () {
 		return (
 			<BrowserRouter>
-			<div style={{textAlign: 'center'}}>
-				<h1> Welcome to React </h1>
-				<img src="images/user.png" style={{height: 50, width: 50}} />
-			</div>
+				<div style={{textAlign: 'center',}}>
+					<h1> Welcome to React </h1>
+					<img src="images/user.png" style={{height: 50, width: 50,}} />
+				</div>
 			</BrowserRouter>
 		);
 	}
