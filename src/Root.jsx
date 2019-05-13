e3003a1ca33256e31c5c7b39910596e7be347b52
Prakash Kandel <unique_prakash2002@yahoo.com>
2019-05-13 22:03:40 +1000

    Initial commit for react redux application

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,14 +1,12 @@
 import { hot, } from 'react-hot-loader/root';
 import React, { Component, } from 'react';
 
-import { BrowserRouter, } from 'react-router-dom';
-
 class Root extends Component {
 	render () {
 		return (
-			<BrowserRouter>
-				<h1> React Redux boilerplate with hot module </h1>
-			</BrowserRouter>
+			<div style={{textAlign: 'center',}}>
+				<h1> React Redux boilerplate with hot module :) </h1>
+			</div>
 		);
 	}
 }
--- src/api/api_url.js
+++
@@ -1,3 +0,0 @@
-export const baseURL = process.env.NODE_ENV === 'development'
-	? 'http://localhost:3000'
-	: 'some production url';
