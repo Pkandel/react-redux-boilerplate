d176ecd1d7cf6e5e65c6a87c87f67fca963bfbe1
Prakash Kandel <prakash.kandel@strategicdata.com.au>
2019-05-14 11:28:19 +1000

    Trying to add server url

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,13 +4,18 @@ import React, { Component, } from 'react';
 import { BrowserRouter, } from 'react-router-dom';
 
 class Root extends Component {
+	componentDidMount() {
+		fetch('/api/todos/1')
+			.then(response => response.json())
+			.then(json => console.log(json));
+	}
 	render () {
 		return (
 			<BrowserRouter>
-			<div style={{textAlign: 'center'}}>
-				<h1> Welcome to React </h1>
-				<img src="images/user.png" style={{height: 50, width: 50}} />
-			</div>
+				<div style={{textAlign: 'center',}}>
+					<h1> Welcome to React </h1>
+					<img src="images/user.png" style={{height: 50, width: 50,}} />
+				</div>
 			</BrowserRouter>
 		);
 	}
