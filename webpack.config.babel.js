aa563a876f13609689558e114e934712e66c744d
Prakash Kandel <prakashk@Prakashs-MacBook-Pro.local>
2019-11-23 10:25:53 +1100

    Remove production config from webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-console */
 import path from 'path';
 import webpack from 'webpack';
-import merge from 'webpack-merge'; // merge webpack config
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin'; // this will automatically insert the js file in index.html
 
 // helper functions
 const resolveRoot = (...args) => path.resolve(__dirname, ...args);
 const resolveSrc = () => path.resolve(__dirname, 'src');
 
 
-const commonConfig = {
+module.exports = {
+	devtool: 'cheap-module-inline-source-map',
+	mode: 'development',
 	entry: {
 		app: resolveSrc(), // we can load third party library in a separate bundle
 	},
@@ -27,36 +26,13 @@ const commonConfig = {
 		},
 		],
 	},
-	plugins: [
-		// this will see the output path of webpack and copy index.html to that place
-		new HtmlWebpackPlugin({
-			template: resolveRoot('public/index.html'),
-			inject: 'body',
-		}),
-		new CopyWebpackPlugin([{
-			from: resolveRoot('public/'),
-			to: resolveRoot('dist/'),
-		}, ]),
-	],
-
 	resolve: {
-		modules: [
-			resolveRoot('node_modules'),
-			resolveSrc(),
-		],
-		extensions: ['.js', '.jsx', '.json', '.css', '.scss', ],
+		extensions: ['.js', '.jsx',],
 		alias: {
 			'react-dom': '@hot-loader/react-dom',
-			// to use alias support in Webstorm we have to mark src as mark directory as resource root
-			'src': resolveSrc(),
 		},
 
 	},
-};
-
-const devConfig = merge(commonConfig, {
-	devtool: 'cheap-module-inline-source-map',
-	mode: 'development',
 	devServer: {
 		contentBase: resolveRoot('public/'),
 		port: 9000,
@@ -66,14 +42,5 @@ const devConfig = merge(commonConfig, {
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
 	],
-});
-const prodConfig = merge(commonConfig, {
-	devtool: 'source-map',
-	mode: 'production',
-});
-
-
-module.exports = (env) => {
-	console.log('Application is running in ' + env.NODE_ENV);
-	return env.NODE_ENV === 'development' ? devConfig : prodConfig;
 };
+
